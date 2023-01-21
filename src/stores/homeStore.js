7b0179638c68c3eacc0660e6fab31e23d7d41a56
3TERRRNITY <qertisww@gmail.com>
2023-01-21 15:42:25 +0300

    add graphics for coins

--- src/stores/homeStore.js
+++ src/stores/homeStore.js
@@ -4,6 +4,7 @@ import debounce from "../helpers/debounce";
 
 const homeStore = create((set) => ({
   coins: [],
+  trending: [],
   query: "",
 
   setQuery: (e) => {
@@ -12,18 +13,25 @@ const homeStore = create((set) => ({
   },
 
   searchCoins: debounce(async () => {
-    const query = homeStore.getState();
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/search?query=${query}`
-    );
-    const coins = res.data.coins.map((coin) => {
-      return {
-        name: coin.name,
-        image: coin.large,
-        id: coin.id,
-      };
-    });
-    set({ coins });
+    const { query, trending } = homeStore.getState();
+
+    if (query.length > 2) {
+      const res = await axios.get(
+        `https://api.coingecko.com/api/v3/search?query=${query}`
+      );
+
+      const coins = res.data.coins.map((coin) => {
+        return {
+          name: coin.name,
+          image: coin.large,
+          id: coin.id,
+        };
+      });
+
+      set({ coins });
+    } else {
+      set({ coins: trending });
+    }
   }, 500),
 
   fetchCoins: async () => {
@@ -39,7 +47,7 @@ const homeStore = create((set) => ({
       };
     });
 
-    set({ coins });
+    set({ coins, trending: coins });
   },
 }));
 
